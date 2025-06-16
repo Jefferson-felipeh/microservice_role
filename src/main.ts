6ff4cdfab1b45358670b2c2ec299f61de8a26991
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-06-16 15:42:38 -0300

    configuração do microservice receptor

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://guest:guest@localhost:5672'],
+      queue: 'ms_role',
+      queueOptions: {
+        durable: true
+      }
+    }
+  });
+
+  app.startAllMicroservices();
+  await app.listen(process.env.PORT ?? 3032);
+}
+bootstrap();
