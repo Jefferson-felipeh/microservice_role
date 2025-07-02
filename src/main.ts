2c9845f1516fdd2a330369e6fd25f1660292268a
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-07-02 17:07:27 -0300

    Definindo o cors para acesso do front aao back

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:4400',
+  });
+
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
